4e62a081d2193598e85cda606b100584b8368226
reimibeta <65539218+reimibeta@users.noreply.github.com>
2021-11-27 22:09:52 +0700

    Update Welcome.js

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,9 +1,19 @@
 import React, { Component } from "react";
+import Test from "./Test";
 
 class Welcome extends Component {
 
     render(){
-        return (<h3>Welcome! Sign up or Sign in!</h3>);
+        return (
+            <div>
+                <h3>Welcome! Sign up or Sign in!</h3>
+                <Test>
+                    <button
+                        onClick={() => console.log("test click!")}
+                    >Test</button>
+                </Test>
+            </div>
+        );
     };
 }
 
