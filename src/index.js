0232f66fbe976a534ba411a3217f1d990dfe0a50
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-27 17:17:13 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM  from "react-dom";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
-
+import reduxThunk from "redux-thunk";
 
 import App from "./App";
 import reducers from "./reducers";
 
+const store = createStore(
+    reducers,
+    {},
+    applyMiddleware(reduxThunk)
+);
+
 ReactDOM.render(
-    <Provider store={createStore(reducers, {})}>
+    <Provider store={store}>
         {/* <React.StrictMode> */}
             <App />
         {/* </React.StrictMode> */}
