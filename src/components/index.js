1c4d8a1c440c14fa3286c0c66a6edad097bbab0f
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-27 13:25:43 +0700

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Signup from "./auth/Signup";
 import Header from "./Header";
 import Welcome from "./Welcome";
 
 const Components = () => {
     return(
-        <BrowserRouter>
+        <BrowserRouter basename="/">
             <Header />
             <Routes>
-                <Route exact path="/" element={<Welcome />} />
+                <Route path="/" element={<Welcome />} />
+                <Route path="/signup" element={<Signup />} />
             </Routes>
         </BrowserRouter>
     );
