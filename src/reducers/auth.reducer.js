83a8b354286dac8d8ee5da1f3c1fb82b1f5c6bb6
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-28 13:33:10 +0700

    Update auth.reducer.js

--- src/reducers/auth.reducer.js
+++ src/reducers/auth.reducer.js
@@ -1,8 +1,14 @@
+import actions from "redux-form/lib/actions";
+import { AUTH_USER } from "../actions/types.action";
+
 const INITIAL_STATE = {
     authenicated: '',
     errorMessage: ''
 };
 
 export default function(state = INITIAL_STATE, action){
+    if (actions.type === AUTH_USER){
+        return { ...state, authenicated: action.payload };
+    }
     return state;
 }
