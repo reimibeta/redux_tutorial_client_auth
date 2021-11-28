f0be69ebd4c4d0823612188fd5b05c1a3e38e172
reimi <65539218+reimibeta@users.noreply.github.com>
2021-11-28 13:33:07 +0700

    Update index.js

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,7 +2,17 @@ import axios from "axios";
 import { AUTH_USER } from "./types.action";
 
 
-export const signup = (formProps) => (dispatch) => {
+export const signup = (formProps) => async (dispatch) => {
     const { username, password } = formProps;
-    axios.post('http://d.pcrpallet.com/api/users/user_token/', { name: username, password: password });
+    axios.post('http://d.pcrpallet.com/api/users/user_token/', 
+    { 
+        name: username, 
+        password: password 
+    }).then(response => {
+        console.log(response.data.token.access);
+        dispatch({ type: AUTH_USER, payload: response.data.token.access });
+    });
+    // const response = axios.post('http://d.pcrpallet.com/api/users/user_token/', { name: username, password: password });
+    // console.log(response);
+    // dispatch({ type: AUTH_USER, payload: response.token.access });
 };
