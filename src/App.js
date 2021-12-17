556f01388f35adb283a8be0b4c83ca0d2d0fae01
reimi <65539218+reimibeta@users.noreply.github.com>
2021-12-17 15:15:29 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Feature from "./components/Feature";
 import Header from "./components/Header";
 import Welcome from "./components/Welcome";
 import Signout from "./components/auth/Signout";
+import requireAuth from "./components/requireAuth";
 
 const App = (props) => {
     // console.log(props.children)
@@ -21,6 +22,7 @@ const App = (props) => {
                     <Route path="/" element={<Welcome />} />
                     <Route path="/signup" element={<Signup />} />
                     <Route path="/feature" element={<Feature />} />
+                    {/* <Route path="/feature" element={requireAuth(Feature)} /> */}
                     <Route path="/signout" element={<Signout />} />
                 </Routes>
             </BrowserRouter>
