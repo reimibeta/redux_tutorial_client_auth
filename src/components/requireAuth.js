53bb051f19d05d0e07a96a3e670554dd16cb19e1
reimi <65539218+reimibeta@users.noreply.github.com>
2021-12-17 15:15:34 +0700

    Update requireAuth.js

--- src/components/requireAuth.js
+++ src/components/requireAuth.js
@@ -56,3 +56,34 @@ export default (ChildComponent) => {
   return connect(mapStateToProps)(WithNavigate);
   // return connect(mapStateToProps)(ComposedComponent);
 };
+
+
+// import React, { Component } from 'react';
+// import { connect } from 'react-redux';
+
+// export default function(ComposedComponent) {
+//     class Authentication extends Component {
+//         static contextTypes = {
+//             router: React.PropTypes === undefined ? 'error' : React.PropTypes.object
+//             // router: React.PropTypes.object
+//         };
+//         componentWillMount() {
+//             if (!this.props.authenticated) {
+//                 this.context.router.push('/');
+//             }
+//         }
+//         componentWillUpdate(nextProps) {
+//             if (!nextProps.authenticated) {
+//                 this.context.router.push('/');
+//             }
+//         }
+//         render() {
+//             return <ComposedComponent {...this.props} />;
+//         }
+//     }
+//     function mapStateToProps({ authenticated }) {
+//         return { authenticated };
+//     }
+
+//     return connect(mapStateToProps)(Authentication);
+// }
